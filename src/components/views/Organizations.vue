66689ff6216a6c3b24d178598b71c4eb4fd26d12
HannahLNguyen <nhuhan.lnguyen@gmail.com>
2023-02-04 15:15:11 -0500

    updated about us

---
+++ src/components/views/Organizations.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h2>
+            Organizations
+        </h2>
+        <h3>
+            SASE
+        </h3>
+        <p>
+            The Society of Asian Scientists and Engineers celebrates diversity in STEM and provides career opportunities as well 
+            as professional development workshops. 
+        </p>
+        <h3>
+            AWC
+        </h3>
+        <p>
+            The Association of Women in Computing
+        </p>
+    </div>
+</template>
+
+<style scoped>
+/*-------------------------------------------*\
+    CSS Normalisation 
+\*-------------------------------------------*/
+
+p {
+    margin: 0;
+    padding: 0;
+}
+</style>
