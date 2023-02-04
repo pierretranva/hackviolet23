4e3d027088a327933b3029e99752821c7136dcf8
HannahLNguyen <nhuhan.lnguyen@gmail.com>
2023-02-04 18:30:26 -0500

    added pics ^_^

--- src/components/views/Organizations.vue
+++ src/components/views/Organizations.vue
@@ -1,45 +1,53 @@
 <template>
     <div>
-        <h2>
+        <h1>
             Organizations
-        </h2>
-        <h3>
+        </h1>
+        <h2>
             SASE
-        </h3>
+        </h2>
         <p>
             The Society of Asian Scientists and Engineers celebrates diversity in STEM and provides career opportunities as well 
             as professional development workshops. Join to connect with mentors, alumni, and form relationships with like minded individuals.
-
+            <br><br>
             "SASE helped me meet with a mentor from a similar cultural background who I formed a personal realtionship with
             and gave me the resources to build an appealing resume." -Pierre Tran
+            <br><br>
+            <img src="../../assets/sase.png"/>
         </p>
-        <h3>
+        <h2>
             AWC
-        </h3>
+        </h2>
         <p>
             The Association of Women in Computing is a nonprofit, student run organization that promotes gender equality in computing.
             Their work includes providing support for women in technological fields and organizing HackViolet, 
             their flagship hackathon. 
+            <br><br>
+            <img src="../../assets/awc.png"/>
         </p>
-        <h3>
+        <h2>
             Hypatia
-        </h3>
+        </h2>
         <p>
             Hypatia is a living learning community for freshman and first-time women engineers. Members of Hypatia are put in mentor groups
             for help with professional development and support in the transition into engineering at Virginia Tech.
-
+            <br><br>
             "The professional development workshops helped me to become a well-rounded engineer." -Jinju Brown
+            <br><br>
+            <img src="../../assets/hypatia.png"/>
         </p>
-        <h3>
+        <h2>
             SWE
-        </h3>
+        </h2>
         <p>
             The Society of Women Engineers contributes to gender inclusivity at Virginia Tech by providing weekly outreach, social, and
             professional development activities. SWE participates in the annual SWE conference which hosts the world's largest career fair
             for women in engineering and technology.
-
+            <br><br>
             "SWE is a great way to connect with female engineers because you don't see them very often within your classes. The female
             speakers and convention help me combat my imposter syndrome." -Vy Dinh
+            <br><br>
+            <img src="../../assets/swe.png"/>
         </p>
     </div>
 </template>
@@ -58,5 +66,25 @@ div{
     display: flex;
     justify-content: center;
     flex-direction: column;
+}
+img {
+    width: 80%;
+    max-width: 300px;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+
+}
+h2 {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+
+}
+h1 {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+
 }
 </style>
