7df0dd1fb1a15f1ffe326dd181d099b02d4a8275
Jinju <jinjub@vt.edu>
2023-02-04 17:34:24 -0500

    alumni

---
+++ src/components/views/Alumni.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h2>
+            Noteable Alumni
+        </h2>
+        <p>
+            Amy Elliot '14
+                She created a 3D-printed statue that was displayed at the Smithsonian for their Women's History Month exibit.
+
+        </p>
+        <p>
+            Carla Rampy '99
+                Currently the President and CEO of Quality Agents
+
+        </p>
+        <p>
+            Lisa Finneran '85
+                Leads a engineer group and runs GDMS engineering
+
+
+        </p>
+        <p>
+            Sarah Frankin '97
+                President and chief marketing officer of Salesforce
+
+        </p>
+        <p>
+            Stacey Branham '07 '14
+                Awarded the 2022 Virginia Tech Department of Computer Science Distinguished Early Career Alumni Awarded
+                
+        </p>
+    </div>
+</template>
+
+<style scoped>
+/*-------------------------------------------*\
+    CSS Normalisation 
+\*-------------------------------------------*/
+
+p {
+    margin: 0;
+    padding: 0;
+}
+</style>
