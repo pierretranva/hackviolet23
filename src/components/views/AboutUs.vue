050bb86f12a986bf58e00029e083e300c811f3e8
pierretranva <pierretranva@gmail.com>
2023-02-05 09:24:33 -0500

    grammer changes

--- src/components/views/AboutUs.vue
+++ src/components/views/AboutUs.vue
@@ -29,13 +29,13 @@
             </div>
             <p>
             <b id="first">Hannah Nguyen:</b> I'm a freshman General Engineering major interested in HCI, technology ethics, and the relationship between race and 
-            technology as master categories. My hobbies include digital art, fashion, and music! I'm a current graphics organizer for VT Hacks 
-            and an active member of both SASE, VSA, and FASA.
+            technology as master categories. My hobbies include digital art, fashion, and music! I'm currently a graphics organizer for VT Hacks 
+            and an active member of SASE, VSA, and FASA.
             <br><br>
             <b id="second"> Vy Dinh:</b> I'm a sophomore majoring in Computer Science with a minor in Mathematics. I'm a part of KASA, SASE, VSA, SWE, and Hokie Ambassadors.
             I hope our project can help future freshmen in engineering adjust to Virginia Tech and grant them the resources to succeed and network!
             <br><br>
-            <b id="third">Pierre Tran:</b> I'm currently a freshman majoring in Computer Science with a minor in Mathematics. I'm a member of SASE, VSA, and FASA. I like to go to the gym, sleep and eat.
+            <b id="third">Pierre Tran:</b> I'm currently a freshman majoring in Computer Science with a minor in Mathematics. I'm a member of SASE, VSA, and FASA. I like to go to the gym, sleep, and eat.
             <br><br>
             <b id="fourth">Jinju Brown:</b> I'm a freshman currently studying General Engineering, but planning to major in Computer Science. I'm a member of CAS, SASE, KASA, 
             and the Galipatia Community. Throughout my years of wanting to become an engineer, I wished that I had a resource like this to help me through
