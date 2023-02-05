7e6a1c1a76120cda28ca6c50ea42e1f853b5ad2c
Jinju <jinjub@vt.edu>
2023-02-04 20:03:38 -0500

    alumni images

--- src/components/views/Alumni.vue
+++ src/components/views/Alumni.vue
@@ -3,31 +3,44 @@
         <h2>
             Noteable Alumni
         </h2>
-        <p>
+        <h4>
             Amy Elliot '14
-                She created a 3D-printed statue that was displayed at the Smithsonian for their Women's History Month exibit.
-
-        </p>
+        </h4>
         <p>
+            She created a 3D-printed statue that was displayed at the Smithsonian for their Women's History Month exibit
+            <img src="../../assets/AmyElliot.jpg"/>
+        </p>
+        <h4>
+            
             Carla Rampy '99
-                Currently the President and CEO of Quality Agents
-
-        </p>
+        </h4>
         <p>
+            Currently the President and CEO of Quality Agents
+            <img src="../../assets/CarlaRampy.jpg"/>
+        </p>
+        <h4>
+            
             Lisa Finneran '85
-                Leads a engineer group and runs GDMS engineering
-
-
-        </p>
+        </h4>
         <p>
+            Leads a engineer group and runs GDMS engineering
+            <img src="../../assets/lisafinneran.jpg"/>
+        </p>
+        <h4>
+            
             Sarah Frankin '97
-                President and chief marketing officer of Salesforce
-
-        </p>
+        </h4>
         <p>
+            President and chief marketing officer of Salesforce
+            <img src="../../assets/SarahFranklin.jpg"/>
+        </p>
+        <h4>
+            
             Stacey Branham '07 '14
-                Awarded the 2022 Virginia Tech Department of Computer Science Distinguished Early Career Alumni Awarded
-                
+        </h4>
+        <p>
+            Awarded the 2022 Virginia Tech Department of Computer Science Distinguished Early Career Alumni Awarded
+            <img src="../../assets/stacybranham.jpg"/>
         </p>
     </div>
 </template>
@@ -40,5 +53,8 @@
 p {
     margin: 0;
     padding: 0;
+}
+img {
+    width: 40%;
 }
 </style>
